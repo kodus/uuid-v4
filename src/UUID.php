98f607eb8fb7119d24b28ff82e949a19e9f7f33a
Rasmus Schultz <rasmus@mindplay.dk>
2017-04-28 10:42:28 +0200

    add UUID v4 validation

--- src/UUID.php
+++ src/UUID.php
@@ -9,22 +9,29 @@ namespace Kodus\Helpers;
  */
 abstract class UUID
 {
-    /**
-     * @type string path to the dev/urandom device on Linux
-     */
-    const DEV_URANDOM = '/dev/urandom';
+    const UUID_V4_PATTERN = '/^[0-9A-F]{8}-[0-9A-F]{4}-4[0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i';
 
     /**
      * Creates a new, random UUID v4
      *
      * @return string UUID v4
      */
-    public static function create()
+    public static function create(): string
     {
         $r = unpack('v*', random_bytes(16));
 
         return sprintf('%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
             $r[1], $r[2], $r[3], $r[4] & 0x0fff | 0x4000,
             $r[5] & 0x3fff | 0x8000, $r[6], $r[7], $r[8]);
+    }
+
+    /**
+     * Validates a given string as UUID v4
+     *
+     * @return bool TRUE, if the given string is a valid UUID v4 identifier
+     */
+    public static function isValid(string $uuid): bool
+    {
+        return preg_match(self::UUID_V4_PATTERN, $uuid) === 1;
     }
 }
--- tests/unit/UUIDCest.php
+++ tests/unit/UUIDCest.php
@@ -7,10 +7,24 @@ use UnitTester;
 
 class UUIDCest
 {
-    const UUID_PATTERN = '/^[0-9A-F]{8}-[0-9A-F]{4}-4[0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i';
+    public function validateUUIDv4(UnitTester $I)
+    {
+        $I->assertTrue(UUID::isValid("afbc8a93-ebc4-41cc-9204-bb52a7534a55"));
+        $I->assertTrue(UUID::isValid("0B886549-8C57-43DD-8D2C-23DCE95BB790"));
+
+        $I->assertFalse(UUID::isValid("B886549-8C57-43DD-8D2C-23DCE95BB79A")); // missing first
+        $I->assertFalse(UUID::isValid("0B886549-8C57-43DD-8D2C-23DCE95BB79")); // missing last
+        $I->assertFalse(UUID::isValid("B886549-8C57-43DD-23DCE95BB79A")); // missing group
+        $I->assertFalse(UUID::isValid("0B886549-8C57-43DD-8D2C-23DCE95BB7901")); // junk at end
+        $I->assertFalse(UUID::isValid("10B886549-8C57-43DD-8D2C-23DCE95BB790")); // junk at start
+        $I->assertFalse(UUID::isValid("5be01114-2bee-11e7-93ae-92361f002671")); // UUID v1
+        $I->assertFalse(UUID::isValid("0B8865498C5743DD8D2C23DCE95BB790")); // missing dashes
+    }
 
     public function createRandomUUID(UnitTester $I)
     {
-        $I->assertRegExp(self::UUID_PATTERN, UUID::create(), "sho nuff looks like a UUID, uh-huh");
+        $uuid = UUID::create();
+
+        $I->assertTrue(UUID::isValid($uuid));
     }
 }
